99822a9edca963658d56d2df07ce4f9feedf0b98
Mathieu <degracia.mathieu@gmail.com>
2020-11-20 21:38:11 +0100

    Add basic auth

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,12 @@
+<form action='/fr/auth/attempt' method='post'>
+	@csrf
+    <div>
+        <input type='text' name='email' value='aloy@gmail.com'>
+    </div>
+    <div>
+        <input type='text' name='password' value='password'>
+    </div>
+    <div>
+        <input type='submit'>
+    </div>
+</form>
--- routes/web.php
+++ routes/web.php
@@ -14,14 +14,25 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::group([
-	'prefix' => LaravelLocalization::setLocale(),
-	'middleware' => [
-		'localeSessionRedirect', 
-		'localizationRedirect', 
-		'localeViewPath',
-	],
+    'prefix' => LaravelLocalization::setLocale(),
+    'middleware' => [
+        'localeSessionRedirect', 
+        'localizationRedirect', 
+        'localeViewPath',
+    ],
 ], function() { 
-	Route::get('/', 'HomeController@index')->name('home');
-	Route::get('posts', 'HomeController@posts')->name('home.posts');
-	Route::get('posts/{hook}', 'HomeController@post')->name('home.post');
+
+    Route::get('/', 'HomeController@index')->name('home');
+    Route::get('posts', 'HomeController@posts')->name('home.posts');
+    Route::get('posts/{hook}', 'HomeController@post')->name('home.post');
+
+    Route::prefix('auth')->as('auth.')->group(function () {
+        Route::middleware('guest')->group(function () {
+            Route::get('login', 'AuthenticateController@login')->name('login');
+            Route::post('attempt', 'AuthenticateController@attempt')->name('attempt');
+        });
+        Route::middleware('auth')->group(function () {
+            Route::get('logout', 'AuthenticateController@logout')->name('logout');
+        });
+    });
 });
