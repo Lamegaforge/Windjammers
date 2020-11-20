fc3c9040e0bfad018d696ef343574db8c1c18cc2
marionj <marionj@focus-home.com>
2020-11-20 15:13:13 +0100

    fix wave

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -8,8 +8,8 @@
     <div x-data="parallaxBackground()" x-ref="bg" @scroll.window="handleScroll" class="absolute top-0 left-0 w-full h-full bg-no-repeat bg-cover -z-1" style="background-image: url('{{ asset('images/beach.jpg')}}');background-position-x: center;"></div>
 </section>
 <section class="relative px-4 pt-16 pb-20 bg-gray-50 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" class="absolute left-0 z-10 -top-16 sm:-top-32 md:-top-36 lg:-top-40 xl:-top-48">
-        <path fill="#f9fafb" fill-opacity="1" d="M0,128L60,117.3C120,107,240,85,360,101.3C480,117,600,171,720,176C840,181,960,139,1080,112C1200,85,1320,75,1380,69.3L1440,64L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 200" class="absolute left-0 z-10 -mt-4 bottom-full">
+        <path fill="#f9fafb" fill-opacity="1" d="M0,128L60,117.3C120,107,240,85,360,101.3C480,117,600,171,720,176C840,181,960,139,1080,112C1200,85,1320,75,1380,69.3L1440,64L1440,200L1380,200C1200,200,1200,200,1080,200C960,200,840,200,720,200C600,200,480,200,360,200C240,200,120,200,60,200L0,200Z"></path>
     </svg>
     <div class="relative z-20 max-w-lg mx-auto lg:max-w-7xl">
         <h2 class="text-3xl font-extrabold tracking-tight text-gray-800 sm:text-4xl">
