34fd04c6becf0daa5962c54eea9e023d168f2c6e
Mathieu <degracia.mathieu@gmail.com>
2020-12-02 13:45:25 +0100

    Update about page

--- resources/views/home/about.blade.php
+++ resources/views/home/about.blade.php
@@ -11,9 +11,9 @@
             <div class="pt-10 pb-8 prose lg:pt-14 max-w-none lg:prose-xl">
                 <img class="max-w-full pb-3 mx-auto" src="{{'/images/beach.jpg'}}" />
                 <p><b>Windjammers France</b> est une communauté de joueurs dédiés au jeu Windjammers.</p>
-                <p>Ce jeu de frisbee atypique a vu le jour en 1994 en arcade, développé par Data East pour la Neo·Geo. Aujourd'hui, en plus du support original et de l'émulation, on peut s'y adonner sur PS4, PSVita et Nintendo Switch grâce à un portage réalisé par Dotemu.</p>
+                <p>Ce jeu de frisbee atypique a vu le jour en 1994 en arcade, développé par <b>Data East pour la Neo·Geo</b>. Aujourd'hui, en plus du support original et de l'émulation, on peut s'y adonner sur <b>PS4, PSVita et Nintendo Switch grâce à un portage réalisé par Dotemu</b>.</p>
                 <p>Créée en 2012 autour de Dan et Wolmar, de plus en plus de joueurs ont rejoint la communauté pour découvrir ou redécouvrir ce jeu qui allie fun immédiat et profondeur compétitive.</p>
-                <p>Des tournois en ligne sur GGPO puis FightCade, nous nous sommes retrouvés à plusieurs reprises lors de divers évènements et notamment le Stunfest et le HFS Summer durant lesquels nous avons pu organiser de magnifiques tournois dont le DiscMania, notre compétition annuelle de référence.</p>
+                <p><b>Des tournois en ligne sur GGPO puis FightCade</b>, nous nous sommes retrouvés à plusieurs reprises lors de divers évènements et notamment le <b>Stunfest et le HFS Summer</b> durant lesquels nous avons pu organiser de magnifiques tournois dont le DiscMania, notre compétition annuelle de référence.</p>
                 <p>Vous trouverez tous les liens pour nous suivre, nous rejoindre, en apprendre davantage sur le jeu en bas de page.</p>
             </div>
         </article>
